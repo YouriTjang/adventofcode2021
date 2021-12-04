f6e767c12f09d2ccaaf114b9abf63b049313bb40
tjangy <youri.tjang@rabobank.nl>
2021-12-04 22:33:10 +0100

    add day3 part2

--- day3_binary_diagnostic.py
+++ day3_binary_diagnostic.py
@@ -24,8 +24,8 @@ def sum_column(report):
 
 
 def calc_gamma(sums, report):
-    majority = len(report)/2
-    gamma = list(map(lambda sum: 1 if sum > majority else 0, sums))
+    majority = len(report) / 2
+    gamma = list(map(lambda sum: 1 if sum >= majority else 0, sums))
     return gamma
 
 
@@ -41,7 +41,18 @@ def binary_list_to_decimal(bin_list):
     return acc
 
 
-def main():
+def bit_criteria_filter(report, co2=False):
+    for i in range(0, len(report)):
+        if len(report) <= 1:
+            return report[0]
+        sums = sum_column(report)
+        gamma = calc_gamma(sums, report)
+        if co2:
+            gamma = inverse_gamma(gamma)
+        report = list(filter(lambda line: line[i] == gamma[i], report))
+
+
+def main_gamma_epsilon():
     lines = read_file()
     report = split_lines(lines)
     sums = sum_column(report)
@@ -49,8 +60,30 @@ def main():
     epsilon = inverse_gamma(gamma)
     gamma_rate = binary_list_to_decimal(gamma)
     epsilon_rate = binary_list_to_decimal(epsilon)
-
     print(gamma_rate * epsilon_rate)
+
+
+def calc_oxygen_generator_rating(report):
+    report = split_lines(report)
+    oxygen_generator_rating = bit_criteria_filter(report)
+    actual = binary_list_to_decimal(oxygen_generator_rating)
+    return actual
+
+
+def calc_co2_scrubber_rating(report):
+    report = split_lines(report)
+    oxygen_generator_rating = bit_criteria_filter(report, co2=True)
+    actual = binary_list_to_decimal(oxygen_generator_rating)
+    return actual
+
+
+def main_life_support_rating():
+    report = read_file()
+    o2 = calc_oxygen_generator_rating(report.copy())
+    print(o2)
+    co2 = calc_co2_scrubber_rating(report)
+    print(co2)
+    print(str(o2 * co2))
 
 
 class MyTestCase(unittest.TestCase):
@@ -89,7 +122,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(expected, split_lines(report))
 
     def test_run_main(self):
-        main()
+        main_gamma_epsilon()
         self.assertTrue(True)
 
     def test_binary_list_to_decimal(self):
@@ -104,6 +137,47 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual([1], calc_gamma([2], [[1], [1], [0]]))
         self.assertEqual([1, 0], calc_gamma([2, 1], [[1], [1], [0]]))
 
+    def test_oxygen_generator_rating(self):
+        report = [
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        ]
+        actual = calc_oxygen_generator_rating(report)
+        self.assertEqual(23, actual)
+
+    def test_co2_scrubber_rating(self):
+        report = [
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        ]
+        actual = calc_co2_scrubber_rating(report)
+        self.assertEqual(10, actual)
+
+    def test_main_life_support_rating(self):
+        main_life_support_rating()
+        self.assertTrue(True)
+
 
 if __name__ == '__main__':
-    unittest.main()
+    main_life_support_rating()
+    # unittest.main()
