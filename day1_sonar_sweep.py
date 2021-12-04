dac3545a564d148195f708d3a155e6d2b645625e
tjangy <youri.tjang@rabobank.nl>
2021-12-04 11:55:20 +0100

    update day1

--- day1_sonar_sweep.py
+++ day1_sonar_sweep.py
@@ -1,5 +1,17 @@
 from functools import reduce
+
 from numpy import inf
+
+
+def sweep_window(sweep):
+    if len(sweep) < 3:
+        return 0
+    for i in range(len(sweep) - 2):
+        yield sweep[i:i+3]
+
+
+def sum_window(windows):
+    return list(map(lambda window: sum(window), windows))
 
 
 def read_file_to_sweep():
@@ -22,7 +34,9 @@ def count_increases(sweep):
 
 def main():
     sweep = read_file_to_sweep()
-    print(count_increases(sweep))
+    windows = list(sweep_window(sweep))
+    windowed_sweeps = sum_window(windows)
+    print(count_increases(windowed_sweeps))
 
 
 if __name__ == '__main__':
--- day1_sonar_sweep_test.py
+++ day1_sonar_sweep_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day1_sonar_sweep import count_increases
+from day1_sonar_sweep import count_increases, sweep_window
 
 
 class MyTestCase(unittest.TestCase):
@@ -17,7 +17,23 @@ class MyTestCase(unittest.TestCase):
             144,
         ]
         actual = count_increases(sweep)
-        self.assertEqual(6, actual)  # add assertion here
+        self.assertEqual(6, actual)
+
+    def test_sweep_window(self):
+        sweep = [
+            134, 138, 142, 143, 141, 142, 145, 140, 144,
+        ]
+
+        actual = list(sweep_window(sweep))
+        self.assertEqual([
+            [134, 138, 142],
+            [138, 142, 143],
+            [142, 143, 141],
+            [143, 141, 142],
+            [141, 142, 145],
+            [142, 145, 140],
+            [145, 140, 144]
+        ], actual)
 
 
 if __name__ == '__main__':
